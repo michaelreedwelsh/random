5a0dc2f32166abdbbf45fc0f0741cd84b538f202
Michael Welsh <michaelreedwelsh@gmail.com>
2016-05-21 12:45:00 -0700

    button no longer refreshes the page. added a slider to change the max value

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-window.onload = function() {
+
+	
+
+
+
+
+
+
 function getRandomNum() {
-	var randomNumber = Math.floor(Math.random() * 100) + 1;
+	var $rangeNum = $("#rangeNum").val();
+	var randomNumber = Math.floor(Math.random() * $rangeNum + 1);
 	document.getElementById("doc").innerHTML = "Your random number is " + randomNumber;
 }
-getRandomNum();
 
-}
+$("#newNumber").click(getRandomNum); 
